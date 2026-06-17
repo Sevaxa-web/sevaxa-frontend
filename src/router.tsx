398de4456db0071273b5782901a5cab273e8ceeb
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-17 16:17:33 +0200

    feat: add page routers

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactUsPage from "./pages/ContactUsPage";
 export const router = createBrowserRouter([
-    {
-        element: <AppLayout/>,
-        children: [
-            {
-                path: "/", element: <HomePage/>
-            }
-        ]
-    }
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/contact",
+        element: <ContactUsPage />,
+      },
+    ],
+  },
 ]);
