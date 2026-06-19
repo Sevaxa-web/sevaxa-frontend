b213c73ca18bca42a431842505c606cd4c2cd863
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-19 21:19:38 +0200

    feat: add IconCard with styles.
    Add subtle box shadow to theme.css, IconCard and Header

---
+++ src/components/ui/IconCard.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from "react";
+
+type IconCardProps = {
+  icon: ReactNode;
+  title: string;
+  description: string;
+};
+
+export default function IconCard({ icon, title, description }: IconCardProps) {
+  return (
+    <div className="icon-card">
+      <div className="icon-container">{icon}</div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
