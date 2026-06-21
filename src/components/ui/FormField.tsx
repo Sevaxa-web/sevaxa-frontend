65c10b45853c94cab9137d3e7dd20ecd428f81c6
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-21 11:56:26 +0200

    feat: add FormField component with styles

---
+++ src/components/ui/FormField.tsx
@@ -0,0 +1,20 @@
+type FormFieldProps = {
+  label: string;
+  type?: string;
+  name: string;
+  placeholder?: string;
+  isMessage?: boolean
+};
+
+export default function FormField({ label, type = "text", name, placeholder, isMessage }: FormFieldProps) {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      {isMessage ? 
+        <textarea name="message" id="message" rows={4} placeholder={placeholder}></textarea>
+        :
+        <input id={name} name={name} type={type} placeholder={placeholder} />
+    }
+    </>
+  );
+}
