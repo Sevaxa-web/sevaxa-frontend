45ab4acf2304a8fcdf4e605301f1bb38963f8734
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-21 14:25:16 +0200

    feat: add hover styles tp header nav

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
-import PageHeader from "../components/ui/PageHeader";
+
 export default function HomePage(){
     return (
-        <><PageHeader title="About" description="Sevaxa was founded with a simple insight: if it's hard to report nonconformities, fewer people will do it. We created a system that removes the thresholds – so that every observation leads to improvement."/></>
+        <></>
     );
 }
