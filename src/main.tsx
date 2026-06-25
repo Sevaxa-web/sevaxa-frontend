2a55c2c6288756f5d0f9c940acfad16339184edc
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-25 20:20:00 +0200

    feat: add form modal context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
-import './theme.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import FormModalContextProvider from "./contexts/FormModalContext";
+import "./theme.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <FormModalContextProvider>
+      <RouterProvider router={router} />
+    </FormModalContextProvider>
   </StrictMode>,
-)
+);
