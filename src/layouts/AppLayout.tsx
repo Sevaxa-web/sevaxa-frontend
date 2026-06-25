928265e662b326fdc0b59fc7fb5a4b43f1d036e3
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-25 20:22:22 +0200

    feat: add modal component to the app

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/Footer/footer";
-
+import FormModal from "../components/modals/FormModal";
 export default function AppLayout() {
   return (
     <div className="app-layout">
       <Header/>
       <main>
         <Outlet />
+        <FormModal />
       </main>
       <Footer />
     </div>
